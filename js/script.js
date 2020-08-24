b8e3c5e11965eaf85d1335c12f5dc1509777663b
Veaceslav Pascaru <pascaruv@gmail.com>
2020-08-24 15:24:51 +0300

    3rd commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var map = L.map('mapid').setView([49.590294, 34.551315], 11);
+var map = L.map('mapid').setView([49.590294, 34.551315], 10);
 
 var spiderbee = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,

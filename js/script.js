43fea08ee858b39d40b04d60ed4e0f9ee1223a75
Veaceslav Pascaru <pascaruv@gmail.com>
2020-08-26 23:47:48 +0300

    Leaflet-Draw added

--- js/script.js
+++ js/script.js
@@ -38,6 +38,7 @@ function styleGeoJson(feature, layer) {
 }
 
 
+
 //for each feature for servants run the function define above - styleGeoJson
 L.geoJson(servants, {
         onEachFeature: styleGeoJson,
@@ -46,6 +47,65 @@ L.geoJson(servants, {
     .addTo(map);
 
 
+    //////////////////////////////////////////////////////////
+    var editableLayers = new L.FeatureGroup();
+    map.addLayer(editableLayers);
+    var MyCustomMarker = L.Icon.extend({
+        options: {
+            shadowUrl: null,
+            iconAnchor: new L.Point(12, 12),
+            iconSize: new L.Point(24, 24),
+            iconUrl: 'link/to/image.png'
+        }
+    });
+    var options = {
+        position: 'topright',
+        draw: {
+            polyline: {
+                shapeOptions: {
+                    color: '#f357a1',
+                    weight: 10
+                }
+            },
+            polygon: {
+                allowIntersection: false, // Restricts shapes to simple polygons
+                drawError: {
+                    color: '#e1e100', // Color the shape will turn when intersects
+                    message: '<strong>Oh snap!<strong> you can\'t draw that!' // Message that will show when intersect
+                },
+                shapeOptions: {
+                    color: '#bada55'
+                }
+            },
+            circle: false, // Turns off this drawing tool
+            rectangle: {
+                shapeOptions: {
+                    clickable: false
+                }
+            },
+            marker: {
+                icon: new MyCustomMarker()
+            }
+        },
+        edit: {
+            featureGroup: editableLayers, //REQUIRED!!
+            remove: false
+        }
+    };
+    
+    var drawControl = new L.Control.Draw(options);
+    map.addControl(drawControl);
+    
+    map.on(L.Draw.Event.CREATED, function (e) {
+        var type = e.layerType,
+            layer = e.layer;
+    
+        if (type === 'marker') {
+            layer.bindPopup('A popup!');
+        }
+    
+        editableLayers.addLayer(layer);
+    });
 // var place = L.marker([51.5, -0.09]).addTo(map)
 //     .bindPopup("<b>Hello world!</b><br />I am a popup.");
 
